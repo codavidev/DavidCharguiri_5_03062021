2f677f148870d12ee02da1c27f8fdea0399b9fc3
David <male.brut@gmail.com>
2021-12-23 07:10:14 +0100

    Affichage des données depuis l'API dans index.html

--- front/js/script.js
+++ front/js/script.js
@@ -9,3 +9,25 @@ const fetchMeuble = async () => {
         console.log(meubleData);
     });
 };
+
+// Affiche les données de l'API sur la page d'Acueil
+const meubleDisplay = async () => {
+    await fetchMeuble();
+
+    document.getElementById(
+        "items",   
+    ).innerHTML = meubleData.map(
+        (meuble) => 
+        `<a href="./product.html?id=${meuble._id}">
+            <article>
+                <img src="${meuble.imageUrl}" alt="${meuble.altTxt}">
+                <h3 class="productName">${meuble.name}</h3>
+                <p class="productDescription">${meuble.description}</p>
+            </article>
+        </a>
+        `,
+        ).join("");// supprime les virgules injectées
+
+};
+// Lance la function pour Afficher les produits
+meubleDisplay();
